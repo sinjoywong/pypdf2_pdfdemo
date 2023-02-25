658d13b9efc1848c8cdd3d8744c2e2c92d2dc7db
sherlockwang <wangxinzhongcn@hotmail.com>
2023-02-25 09:51:16 +0800

    works, need to remove blank pages

--- main.py
+++ main.py
@@ -1,46 +1,58 @@
-from PyPDF2 import PdfReader, PdfMerger, PdfWriter, Transformation
+from PyPDF2 import PdfReader, PdfMerger, PdfWriter, Transformation, PaperSize
 
 # https://pypdf2.readthedocs.io/en/latest/user/cropping-and-transforming.html
 
-pdf = PdfReader(r'./report-dec2021.pdf')
+pdf = PdfReader(r'./CR.pdf')
 num_pages = len(pdf.pages)
 
 pdf_writer = PdfWriter()
-#pdf_writer.add_page(pdf.pages[0].rotate(90))
+# pdf_writer.add_page(pdf.pages[0].rotate(90))
 
 
 pdf_write_merge = PdfMerger()
 
 # common:
-op_translate_to_right_half_of_a4_paper = Transformation().translate(149,0)
-op_scale_to_half = Transformation().scale(sx=0.25, sy=0.25)
+op_translate_to_right_half_of_a4_paper = Transformation().translate(PaperSize.A5.height / 2, 0)
+op_scale_to_half = Transformation().scale(sx=0.5, sy=0.5)
+
 
 def merge_every_two_page_in_one_page(num_pages):
-    for i in range(num_pages):
-        if i % 2 == 0:
-            print ("-----%d",i)
+    last_part_index = -1
+    blank_page = pdf_writer.add_blank_page(PaperSize.A5.height, PaperSize.A5.width)
+    print("num_pages=%d", num_pages)
+    for i in range(0, num_pages, 2):
+        blank_page = pdf_writer.add_blank_page(PaperSize.A5.height, PaperSize.A5.width)
+        print("-----%d", i)
+        page_odd = pdf.pages[i]
+        page_odd.add_transformation(op_scale_to_half)
+        print("======%d", i + 1)
+        if i + 1 < num_pages:
+            page_even = pdf.pages[i + 1]
+            page_even.add_transformation(op_scale_to_half)
+            page_even.add_transformation(op_translate_to_right_half_of_a4_paper)
+
+            blank_page.merge_page(page_odd)
+            blank_page.merge_page(page_even)
+            pdf_writer.add_page(blank_page)
         else:
-            print ("======%d",i)
+            last_part_index = num_pages - 1
+            break
 
-page_odd = pdf.pages[0]
-page_odd.add_transformation(op_scale_to_half)
+    if last_part_index > 0:
+        #blank_page = pdf_writer.add_blank_page(PaperSize.A5.height, PaperSize.A5.width)
+        page_last_part = pdf.pages[last_part_index]
+        #page_last_part.add_transformation(op_scale_to_half)
+        blank_page.merge_page(page_last_part)
+        pdf_writer.add_page(blank_page)
 
-page_even = pdf.pages[1]
-page_even.add_transformation(op_scale_to_half)
-page_even.add_transformation(op_translate_to_right_half_of_a4_paper)
-#page_even_rotated = page_even.rotate(0)
+
+
+# page_even_rotated = page_even.rotate(0)
 
 # add blank page of A4 size:
-blank_page = pdf_writer.add_blank_page(297,210)
-blank_page.merge_page(page_odd)
-blank_page.merge_page(page_even)
 
+# num_pages = 2
 merge_every_two_page_in_one_page(num_pages)
 
-#pdf_writer.add_page(blank_page)
-pdf_writer.add_page(blank_page)
-with open(r'report-dec2021-new.pdf', 'wb') as f:
+with open(r'CR-new.pdf', 'wb') as f:
     pdf_writer.write(f)
-
-
-
---
+++ remove_passwd.py
@@ -0,0 +1,109 @@
+import argparse
+import os
+import shutil
+import stat
+import time
+
+import PyPDF2
+
+# https://github.com/doolieSoft/PdfPasswordRemover/blob/master/PdfPasswordRemover.py
+def main():
+    parser = argparse.ArgumentParser(
+        description="Tool made to remove password set on pdf files - (Stefano Crapanzano - s.crapanzano@gmail.com)")
+    parser.add_argument('source_directory', help='Directory containing the PDFs with password protection')
+    parser.add_argument('destination_directory', help='Directory where the new PDFs will be placed')
+    parser.add_argument('-a', '--age_of_file_to_treat', help='Age in seconds of file to be treated')
+    args = parser.parse_args()
+    source_directory = vars(args)['source_directory']
+    destination_directory = vars(args)['destination_directory']
+    age_of_file_to_treat = vars(args)['age_of_file_to_treat']
+
+    timestamp = time.localtime()
+    date_exec = str(
+        timestamp.tm_year) + str(timestamp.tm_mon).zfill(2) + str(timestamp.tm_mday).zfill(2) + str(
+        timestamp.tm_hour).zfill(2) + str(
+        timestamp.tm_min).zfill(2) + str(timestamp.tm_sec).zfill(2)
+
+    destination_backup_folder_name = os.path.join(destination_directory, date_exec + '_backup')
+    destination_new_folder_name = os.path.join(destination_directory, date_exec + '_new')
+
+    nb_pdf_corrected = 0
+    nb_files_moved = 0
+
+    for file_name in os.listdir(source_directory):
+
+        source_file_path = os.path.join(source_directory, file_name)
+
+        if not os.path.isfile(source_file_path):
+            continue
+
+        if age_of_file_to_treat and file_age_in_seconds(source_file_path) < float(age_of_file_to_treat):
+            continue
+
+        try:
+            if not os.path.isdir(destination_backup_folder_name):
+                os.mkdir(destination_backup_folder_name)
+        except:
+            print("Exception occurred while creating " + destination_backup_folder_name)
+            break
+
+        try:
+            if not os.path.isdir(destination_new_folder_name):
+                os.mkdir(destination_new_folder_name)
+        except:
+            print("Exception occurred while creating " + destination_new_folder_name)
+            break
+
+        old_file_name = file_name
+        file_name = file_name.lower().replace('.pdf.convert', '.pdf')
+        file_name = file_name.lower().replace('.pdf.import', '.pdf')
+        destination_file_path = os.path.join(destination_new_folder_name, file_name)
+
+        if file_name.lower().endswith(('.pdf', '.pdf.convert', 'pdf.import')):
+            f = None
+            pdf_output_file = None
+
+            try:
+                f = open(source_file_path, 'rb')
+
+                file_reader = PyPDF2.PdfReader(f)
+
+                print(source_file_path + " > Nb pages: " + str(len(file_reader.pages)) + "; Is Encrypted: " + str(
+                    file_reader.is_encrypted))
+
+                pdf_writer = PyPDF2.PdfWriter()
+
+                for pageNum in range(len(file_reader.pages)):
+                    page_obj = file_reader.pages[pageNum]
+                    pdf_writer.add_page(page_obj)
+
+                pdf_output_file = open(destination_file_path, 'wb')
+                pdf_writer.write(pdf_output_file)
+                pdf_output_file.close()
+                nb_pdf_corrected = nb_pdf_corrected + 1
+                f.close()
+            except:
+                if f is not None:
+                    f.close()
+
+                if pdf_output_file is not None:
+                    pdf_output_file.close()
+
+                print("--- Exception occurred with creation of file " + destination_file_path + " ---")
+        try:
+            shutil.move(source_file_path, os.path.join(destination_backup_folder_name, old_file_name))
+            nb_files_moved = nb_files_moved + 1
+        except:
+            print("Exception occurred while moving from [ " + source_file_path + " ] to [ " +
+                  destination_backup_folder_name + old_file_name + " ]")
+
+    print(str(nb_files_moved) + " file(s) have been moved in " + destination_backup_folder_name)
+    print(str(nb_pdf_corrected) + " pdf file(s) have been treated.")
+    input("Press enter to continue...")
+
+def file_age_in_seconds(pathname):
+    return time.time() - os.stat(pathname)[stat.ST_MTIME]
+
+
+if __name__ == "__main__":
+    main()
