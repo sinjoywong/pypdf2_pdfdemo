8afe781d3dc5b13d04d3ca82fe38ea639f19cf2c
sherlockwang <wangxinzhongcn@hotmail.com>
2023-02-26 15:28:11 +0800

    update

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-from PyPDF2 import PdfReader, PdfMerger, PdfWriter, Transformation, PaperSize
+from PyPDF2 import PdfReader, PdfWriter, Transformation, PaperSize
 import math,os
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 
 # https://pypdf2.readthedocs.io/en/latest/user/cropping-and-transforming.html
 
-os.makedirs("tmp",exist_ok=True)
-original_pdf_name = "EVEN.pdf"
-tmp_pdf_name = "tmp/tmp_" + original_pdf_name
-final_pdf_name = "final." + original_pdf_name
-
-# common:
-op_translate_to_right_half_of_a4_paper = Transformation().translate(PaperSize.A5.height / 2, 0)
-op_scale_to_half = Transformation().scale(sx=0.5, sy=0.5)
-
-
 def merge_every_two_page_in_one_page():
+    op_translate_to_right_half_of_a4_paper = Transformation().translate(PaperSize.A5.height / 2, 0)
+    op_scale_to_half = Transformation().scale(sx=0.5, sy=0.5)
     pdf = PdfReader(original_pdf_name)
     num_pages = len(pdf.pages)
     pdf_writer = PdfWriter()
@@ -34,7 +29,6 @@ def merge_every_two_page_in_one_page():
 
             blank_page.merge_page(page_odd)
             blank_page.merge_page(page_even)
-            # pdf_writer.add_page(blank_page)
             pdf_writer.insert_page(blank_page, index)
             index = index + 1
         else:
@@ -59,10 +53,44 @@ def finalize():
         pdf_writer.write(f)
     os.remove(tmp_pdf_name)
 
-
+original_pdf_name = ""
+tmp_pdf_name = original_pdf_name + ".tmp"
+final_pdf_name = original_pdf_name + "_final.pdf"
 def main():
     merge_every_two_page_in_one_page()
     finalize()
+    messagebox.showinfo('处理完成', '请在源文件相同目录下找到以_final.pdf结尾的同名文件，并仔细检查是否符合预期！')
+    open_directory()
+
+original_pdf_name = ""
+def get_file_path():
+    global original_pdf_name
+    global tmp_pdf_name
+    global final_pdf_name
+    original_pdf_name = filedialog.askopenfilename(title="Select A File", filetypes=(("pdf", "*.pdf"),))
+    tmp_pdf_name = original_pdf_name + ".tmp"
+    final_pdf_name = original_pdf_name + "_final.pdf"
+    l1 = tk.Label(text="已选文件: " + original_pdf_name).pack()
+
+def open_directory():
+    global final_pdf_name
+    dir_name = os.path.dirname(final_pdf_name)
+    print(final_pdf_name)
+    print(dir_name)
+    #os.system('start ' + dir_name)
+    if os.sys.platform == "win32":
+        os.startfile(dir_name)
+    elif os.sys.platform == "darwin":
+        print("not supported in mac system")
+    else:
+        print("not support in system: " + os.sys.platform)
 
 
-main()
+window = tk.Tk()
+window.title("PDF处理小工具 v0.1")
+label = tk.Label(text="功能：将每两页A4纸pdf横向放置合并为一页A4纸.")
+label.pack()
+
+button = tk.Button(text="打开pdf文件", command=get_file_path).pack(pady=10)
+button = tk.Button(text="开始处理", command=main).pack()
+window.mainloop()
